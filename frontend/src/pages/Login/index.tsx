980542019e975a2f0381bc9058eab23730d18fc9
letnull19a <alexvolkovdd@gmail.com>
2025-02-22 11:09:21 +0300

    feat: added sending form mechanic

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,14 +1,39 @@
 import { Panel, Button, Input } from '@ui'
+import { useForm, Controller } from 'react-hook-form'
 import styles from './style.module.scss'
 
 export const LoginPage = () => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      login: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
+    // call to api.auth()
+  }
+
   return (
     <Panel className={styles.authPanel}>
       <h2>Войти</h2>
-      <Input errorMessage='some error' label='Логин' />
-      <Input isPassword label='Пароль' />
-      <Button label='Войти' />
-      <span className={styles.accountManage}>Создать аккаунт</span>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name='login'
+          control={control}
+          render={({ field }) => (
+            <Input {...field} errorMessage='some error' label='Логин' />
+          )}
+        />
+        <Controller
+          name='password'
+          control={control}
+          render={({ field }) => <Input {...field} isPassword label='Пароль' />}
+        />
+        <Button label='Войти' />
+        <span className={styles.accountManage}>Создать аккаунт</span>
+      </form>
     </Panel>
   )
 }
--- frontend/src/shared/ui/input/index.tsx
+++ frontend/src/shared/ui/input/index.tsx
@@ -1,15 +1,18 @@
-import styles from './style.module.scss'
+import { forwardRef } from 'react'
 import { InputText } from 'primereact/inputtext'
+import styles from './style.module.scss'
 
 type TInputProps = {
   label: string
   isError: boolean
   isPassword: boolean
   errorMessage: string
+  value: string
+  onChange: (e) => void
 }
 
-export const Input = (props: Partial<TInputProps>) => {
-  const { label, isError, isPassword, errorMessage } = props
+export const Input = forwardRef<Partial<TInputProps>>((props, ref) => {
+  const { label, isError, isPassword, errorMessage, onChange, value } = props
 
   return (
     <div
@@ -19,10 +22,13 @@ export const Input = (props: Partial<TInputProps>) => {
     >
       <span className={styles.inputLabel}>{label}</span>
       <InputText
+        onChange={onChange}
+        value={value}
+        ref={ref}
         className={styles.input}
         type={isPassword ? 'password' : 'text'}
       />
       {isError && <span className={styles.errorMessage}>{errorMessage}</span>}
     </div>
   )
-}
+})
