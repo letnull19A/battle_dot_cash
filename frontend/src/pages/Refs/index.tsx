45f7e401e79b0c825311b494fa3c3e967da117f4
letnull19a <alexvolkovdd@gmail.com>
2025-02-24 20:48:50 +0300

    feat: referal page

---
+++ frontend/src/pages/Refs/index.tsx
@@ -0,0 +1,11 @@
+import { InviteLinkCard } from './InviteLinkCard'
+import { RefsList } from './RefsList'
+
+export const RefsPage = () => {
+  return (
+    <div className={'pt-4'}>
+      <InviteLinkCard />
+      <RefsList />
+    </div>
+  )
+}
